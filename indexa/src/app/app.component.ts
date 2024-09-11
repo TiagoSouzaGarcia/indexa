0ef3b9ec822d7ab462521a57e7dcf598216e704c
Tiago Souza Garcia <tiago.s.garcia@gmail.com>
2024-09-11 13:49:12 -0300

    Add files via upload

---
+++ indexa/src/app/app.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './components/container/container.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './components/separador/separador.component';
+import { ContatoComponent } from './components/contato/contato.component';
+
+interface Contato {
+  id: number;
+  nome: string;
+  telefone: string;
+}
+
+import agenda from './agenda.json';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent,
+    FormsModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Contato[] = agenda;
+  filtroPorTexto: string = '';
+
+  filtrarContatosPorTexto(): Contato[] {
+    if (!this.filtroPorTexto) {
+      return this.contatos;
+    }
+    this.filtroPorTexto = this.filtroPorTexto
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '');
+
+    return this.contatos.filter((contato) => {
+      return contato.nome
+        .toLowerCase()
+        .includes(this.filtroPorTexto.toLowerCase());
+    });
+  }
+
+  filtrarContatosPorLetraInicial(letra: string): Contato[] {
+    return this.filtrarContatosPorTexto().filter((contato) => {
+      return contato.nome.toLowerCase().startsWith(letra);
+    });
+  }
+}
